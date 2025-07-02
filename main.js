4c152288405017ca7b31105d32c9701887ae3673
moniktamburi <monik.araujo@escola.pr.gov.br>
2025-07-02 20:42:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+Const numeroSenha = document.querySelector("paramentro-senha_texto");
+let tamanhoSenha = 12;
+numeroSenha.texto.textcontent = tamanhoSenha;
+Const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+conts numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll
