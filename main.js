c1e26f34f653456e172e1bf6f9f7af1c2f63ec3c
moniktamburi <monik.araujo@escola.pr.gov.br>
2025-08-14 19:07:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,37 @@ const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 conts numeros = '0123456789';
 const simbolos = '!@%*?';
 const botoes = document.querySelectorAll
+const campoSenha = document.querySelectorAll('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelectorAll('forca');
+
+
+ botoes[0].onclick = diminuiTamanho;
+ botoes[1].onclick = aumentaTamanho;
+
+ function diminuiTamanho(){
+    if(tamanhoSenha >1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+ }
+
+ function aumentaTamanho(){
+    if(tamanhoSenha <20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+ }
+
+
+ function gerasenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) 
